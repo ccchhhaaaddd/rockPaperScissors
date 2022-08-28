3fb2a2cb69f23f600bb43d9327dff86e9ae5f1c8
ccchhhaaaddd <chad.colston@gmail.com>
2022-08-28 15:49:57 -0400

    score counter without looping

--- script.js
+++ script.js
@@ -12,44 +12,77 @@ function getComputerChoice() {
 
 let computerSelection = getComputerChoice();
 
-// prompt player for their choice
+// prompt player for their choice and then standardize the string input
 
 let playerSelection = window.prompt('Please type Rock, Paper, or Scissors');
+
+let fixedPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
 /* take computer choice, player choice, and play one round 
 of the game - returning a string that declares the winner 
 (example: "You lose Paper beats Rock") */
 
-function singleRound(computerSelection, playerSelection) {
-    if (computerSelection ==='Rock' && playerSelection ==='Rock') {
-        alert ("Tie, both selected Rock");
-    } else if (computerSelection ==='Paper' && playerSelection ==='Paper') { 
-        alert ("Tie, both selected Paper");
-    } else if (computerSelection ==='Scissors' && playerSelection ==='Scissors') { 
-        alert ("Tie, both selected Scissors");
+function singleRound(computerSelection, fixedPlayerSelection) {
+    if (computerSelection ==='Rock' && fixedPlayerSelection ==='Rock') {
+        return ("Tie, both selected Rock");
+    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Paper') { 
+        return ("Tie, both selected Paper");
+    } else if (computerSelection ==='Scissors' && fixedPlayerSelection ==='Scissors') { 
+        return ("Tie, both selected Scissors");
 
-    } else if (computerSelection ==='Rock' && playerSelection ==='Scissors') { 
-        alert ("You lose Rock beats Scissors");
-    } else if (computerSelection ==='Rock' && playerSelection ==='Paper') { 
-        alert ("You win Paper beats Rock");
+    } else if (computerSelection ==='Rock' && fixedPlayerSelection ==='Scissors') { 
+        return ("You lose Rock beats Scissors");
+    } else if (computerSelection ==='Rock' && fixedPlayerSelection ==='Paper') { 
+        return ("You win Paper beats Rock");
 
-    } else if (computerSelection ==='Paper' && playerSelection ==='Rock') { 
-        alert ("You lose Paper beats Rock");
-    } else if (computerSelection ==='Paper' && playerSelection ==='Scissors') { 
-        alert ("You win Scissors beats Paper");
+    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Rock') { 
+        return ("You lose Paper beats Rock");
+    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Scissors') { 
+        return ("You win Scissors beats Paper");
 
-    } else if (computerSelection ==='Scissors' && playerSelection ==='Rock') { 
-        alert ("You win Rock beats Scissors");
+    } else if (computerSelection ==='Scissors' && fixedPlayerSelection ==='Rock') { 
+        return ("You win Rock beats Scissors");
     } else { 
-        alert ("You lose Scissors beats Paper");
+        return ("You lose Scissors beats Paper");
     };
+
 }
 
-alert(singleRound(computerSelection,playerSelection));
+alert(singleRound(computerSelection,fixedPlayerSelection));
 
 // create game() function that keeps score and reports a winner/loser
 
+
 function game() {
+    for (let i = 0; i < 5; i++) {
+        singleRound();
+    }
+    let playerScore = 0;
+    let computerScore = 0;
+    if (computerSelection ==='Rock' && fixedPlayerSelection ==='Rock') {
+        alert ("No Change in score.");
+    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Paper') { 
+        alert ("No Change in score.");
+    } else if (computerSelection ==='Scissors' && fixedPlayerSelection ==='Scissors') { 
+        alert ("No Change in score.");
+
+    } else if (computerSelection ==='Rock' && fixedPlayerSelection ==='Scissors') { 
+        alert ("Computer score is: " + (computerScore += 1));
+    } else if (computerSelection ==='Rock' && fixedPlayerSelection ==='Paper') { 
+        alert ("Player score is : " + (playerScore += 1));
+
+    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Rock') { 
+        alert ("Computer score is: " + (computerScore += 1));
+    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Scissors') { 
+        alert ("Player score is : " + (playerScore += 1));
+
+    } else if (computerSelection ==='Scissors' && fixedPlayerSelection ==='Rock') { 
+        alert ("Player score is : " + (playerScore += 1));
+    } else { 
+        alert ("Computer score is: " + (computerScore += 1));
+    };
 
 }
+
+game()
 
