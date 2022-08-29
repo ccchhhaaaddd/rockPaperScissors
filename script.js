b8e1bdd1abababcdbfce770649e56701d553ccff
ccchhhaaaddd <chad.colston@gmail.com>
2022-08-28 21:06:18 -0400

    removed global variables in favour of functions and made game run better, but not perfect.

--- script.js
+++ script.js
@@ -1,88 +1,70 @@
-// establish game choice list and randomizer
-
-const choices = ["Rock", "Paper", "Scissors"];
-
-const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-
 // determine the computer choice (randomly decided)
 
 function getComputerChoice() {
-    return randomChoice
+    let choices = ["Rock", "Paper", "Scissors"];
+    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
 }
-
-let computerSelection = getComputerChoice();
 
 // prompt player for their choice and then standardize the string input
 
-let playerSelection = window.prompt('Please type Rock, Paper, or Scissors');
-
-let fixedPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+function getPlayerChoice() {
+    let playerSelection = window.prompt('Please type Rock, Paper, or Scissors');
+    let fixedPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    return fixedPlayerSelection;
+}
 
 /* take computer choice, player choice, and play one round 
 of the game - returning a string that declares the winner 
 (example: "You lose Paper beats Rock") */
 
-function singleRound(computerSelection, fixedPlayerSelection) {
-    if (computerSelection ==='Rock' && fixedPlayerSelection ==='Rock') {
-        return ("Tie, both selected Rock");
-    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Paper') { 
-        return ("Tie, both selected Paper");
-    } else if (computerSelection ==='Scissors' && fixedPlayerSelection ==='Scissors') { 
-        return ("Tie, both selected Scissors");
+function singleRound() {
+    let computer = getComputerChoice();
+    let player = getPlayerChoice();
 
-    } else if (computerSelection ==='Rock' && fixedPlayerSelection ==='Scissors') { 
-        return ("You lose Rock beats Scissors");
-    } else if (computerSelection ==='Rock' && fixedPlayerSelection ==='Paper') { 
-        return ("You win Paper beats Rock");
+    if (player === computer) {
+        console.log("Tie Game");
+        return ("Tie Game");
 
-    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Rock') { 
-        return ("You lose Paper beats Rock");
-    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Scissors') { 
-        return ("You win Scissors beats Paper");
+    } else if (computer ==='Rock' && player ==='Scissors') { 
+        console.log ("You lose Rock beats Scissors");
+        return ("Computer Wins");
+    } else if (computer ==='Rock' && player ==='Paper') { 
+        console.log ("You win Paper beats Rock");
+        return ("Player Wins");
 
-    } else if (computerSelection ==='Scissors' && fixedPlayerSelection ==='Rock') { 
-        return ("You win Rock beats Scissors");
+    } else if (computer ==='Paper' && player ==='Rock') { 
+        console.log ("You lose Paper beats Rock");
+        return ("Computer Wins");
+    } else if (computer ==='Paper' && player ==='Scissors') { 
+        console.log ("You win Scissors beats Paper");
+        return ("Player Wins");
+
+    } else if (computer ==='Scissors' && player ==='Rock') { 
+        console.log ("You win Rock beats Scissors");
+        return ("Player Wins");
     } else { 
-        return ("You lose Scissors beats Paper");
+        console.log ("You lose Scissors beats Paper");
+        return ("Computer Wins");
     };
 
 }
 
-alert(singleRound(computerSelection,fixedPlayerSelection));
-
 // create game() function that keeps score and reports a winner/loser
-
+//THERE is problems with the below code
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        singleRound();
-    }
-    let playerScore = 0;
-    let computerScore = 0;
-    if (computerSelection ==='Rock' && fixedPlayerSelection ==='Rock') {
-        alert ("No Change in score.");
-    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Paper') { 
-        alert ("No Change in score.");
-    } else if (computerSelection ==='Scissors' && fixedPlayerSelection ==='Scissors') { 
-        alert ("No Change in score.");
+        let outcome = singleRound();
+        let playerScore = 0;
+        let computerScore = 0;
+        if (outcome === "Tie Game") {
+            console.log ("No Change in score.");
+        } else if (outcome === "Player Wins") { 
+            console.log ("Player score is: " + (playerScore += 1));
+        } else { 
+            console.log ("Computer score is: " + (computerScore += 1));
+        }; 
+    }}
 
-    } else if (computerSelection ==='Rock' && fixedPlayerSelection ==='Scissors') { 
-        alert ("Computer score is: " + (computerScore += 1));
-    } else if (computerSelection ==='Rock' && fixedPlayerSelection ==='Paper') { 
-        alert ("Player score is : " + (playerScore += 1));
-
-    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Rock') { 
-        alert ("Computer score is: " + (computerScore += 1));
-    } else if (computerSelection ==='Paper' && fixedPlayerSelection ==='Scissors') { 
-        alert ("Player score is : " + (playerScore += 1));
-
-    } else if (computerSelection ==='Scissors' && fixedPlayerSelection ==='Rock') { 
-        alert ("Player score is : " + (playerScore += 1));
-    } else { 
-        alert ("Computer score is: " + (computerScore += 1));
-    };
-
-}
-
-game()
-
+game();
